89ccbe8b2ab08ed51cde016c16772b4a72c142c5
marv26 <cianoprimario2202@gmail.com>
2022-04-20 11:17:42 +0200

    (almost-fix): prisma connection

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,21 +1,20 @@
+import { UserModel } from './../../../models/user';
 import { NextApiRequest, NextApiResponse } from "next";
 import { connect } from '../../../utils/dbConnect';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import { encryptPasswordSync } from "../../../utils/crypto";
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-    const payload = req.body;
-    //await prisma.$connect();
-
-    try {
-        payload.password = encryptPasswordSync(payload.password);
+    const payload: UserModel = req.body;
+    payload.password = encryptPasswordSync(payload.password);
         
-        const { db } = await connect();
-        const user = await db.collection('users').insertOne(payload);
-        //const user = await prisma.user.create({ data: payload });
+    try {
+        await prisma.$connect();
+        await prisma.user.create({ data: payload });
+        await prisma.$disconnect();
         
         return res.status(200).json({ message: "Account creato con successo", status: 200 })
     } catch (error) {
